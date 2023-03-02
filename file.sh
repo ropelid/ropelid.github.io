438e7fdbdffda1ce4f36f132d2f75ca0f3b898b6
arfarell silva Ramadhan <33674500+ropelid@users.noreply.github.com>
2023-03-02 08:47:36 +0700

    file

---
+++ file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# File: file.sh 
+# install htop  and screen
+sudo apt-get install htop screen -y
+#  go to home
+cd ~/home
+# install uplink 
+
+curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
+unzip -o uplink_linux_amd64.zip
+sudo install uplink /usr/local/bin/uplink
+curl https://raw.githubusercontent.com/ropelid/ropelid.github.io/master/file.txt -o file.txt
+uplink access import main accessgrant.txt
+# copy 
+uplink cp file.txt sj://bucket/file.txt
